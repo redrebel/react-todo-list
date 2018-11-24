d381d9b02df4228ae1afddd209fe989545bf6764
Lee, Jung Geun <cjred77@gmail.com>
2018-11-24 22:18:24 +0900

    commit

---
+++ todo-list-1/src/components/TodoItemList.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import TodoItem from './TodoItem'
+
+class TodoItemList extends Component{
+  render(){
+    const { todos, onToggle, onRemove} = this.props;
+
+    const todoList = todos.map(
+      ({id, text, checked}) => (
+        <TodoItem
+          id={id}
+          text={text}
+          checked={checked}
+          onToggle={onToggle}
+          onRemove={onRemove}
+          key={id}
+        />
+      )
+    );
+
+    return (
+      <div>
+        <div>
+          {todoList}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default TodoItemList;
